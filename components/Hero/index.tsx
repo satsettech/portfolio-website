cd2241e6086406ee3f4246edb812f3c8e0cba231
Fazrimo <fazriansyah@kokatto.com>
2025-01-03 20:25:26 +0700

    adjust menu header to scroll down

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -37,7 +37,13 @@ const Hero = () => {
                     Get Started
                   </Link>
                   <Link
-                    href="/about"
+                    href="#about"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      document.querySelector("#about")?.scrollIntoView({
+                        behavior: "smooth"
+                      });
+                    }}
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     About Us
